57cee255ce5bbdd251550efffe1ae7d79dd37d5d
monority <chenu.pro@gmail.com>
2023-05-09 10:24:38 +0200

    Fusion des deux et fix class

---
+++ js/main.js
@@ -0,0 +1,86 @@
+console.log("test");
+
+let i = 0;
+
+// création du texte à afficher
+let newP = document.createElement('p');
+let placediv = document.getElementById("affichage");
+let sectionLeave = document.getElementById("sectionFacture");
+// ---
+
+
+let enregistrement = [{
+    prenom: "",
+    nom: "",
+    nuits: "",
+    chambre: "",
+    dejeuner: "",
+    prix: ""
+}];
+
+
+
+function displayRadioValue() {
+    var x = document.querySelector('input[name="petit_dej"]:checked')  ;
+    return x.value;
+}
+
+
+function getValue() {
+    // Sélectionner l'élément input et récupérer sa valeur
+    let prenom = document.getElementById("prenom").value;
+    let nom = document.getElementById("nom").value;
+    let nuits = document.getElementById("nuits").value;
+    let chambre = document.getElementById("typechambre").value;
+    let dej = displayRadioValue();
+
+
+    // Stocker dans tableau enregistrement
+    enregistrement[i] = {
+        prenom: prenom,
+        nom: nom,
+        nuits: nuits,
+        chambre: chambre,
+        dejeuner: dej
+    };
+    i++;
+
+    // optionnel : affichage
+    console.table(enregistrement);
+    alert("tu es enregistré");
+}
+
+
+
+function search() {
+    let prenom2 = document.getElementById("prenomSearch").value;
+    let nom2 = document.getElementById("nomSearch").value;
+    let trouve = false;
+
+    placediv.append(newP);
+
+
+    for (let searchindex = 0; searchindex < enregistrement.length; searchindex++) {
+        if (enregistrement[searchindex].prenom === prenom2 && enregistrement[searchindex].nom === nom2 && enregistrement[searchindex].prenom !== "") {
+            console.log("c bon");
+            trouve = true;
+
+            // calcul du prix
+            if (enregistrement[searchindex].chambre === "ch1") {
+                enregistrement[searchindex].prix = 65;
+            } else if (enregistrement[searchindex].chambre === "ch2") {
+                enregistrement[searchindex].prix = 89;
+            } else if (enregistrement[searchindex].chambre === "ch3") {
+                enregistrement[searchindex].prix = 139;
+            } else enregistrement[searchindex].prix = 189;
+            // ---
+
+            newP.textContent = (`La facture de ${enregistrement[searchindex].prenom} ${enregistrement[searchindex].nom} est de ${enregistrement[searchindex].prix}€`);
+        }
+        
+        else {
+            trouve = false;
+            newP.textContent = (`Client introuvable`);
+        }
+    }
+}
